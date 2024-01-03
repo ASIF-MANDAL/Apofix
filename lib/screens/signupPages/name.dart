1877468d52a3e6f90d36dc72894fbfd2104268f7
Dice_App <asifmandal67@gmail.com>
2024-01-03 14:51:04 +0530

    Initial

---
+++ lib/screens/signupPages/name.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../consts/onboardingwidgets.dart';
+
+class Name extends StatelessWidget {
+  const Name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(left: 24.w, top: 90.h, right: 24.w),),
+          Row(
+            children: [
+              SizedBox(width: 16.w,),
+              Text(
+                  'Hi,\nEnter your Name',
+                  style: GoogleFonts.inter(
+                    fontSize: 32.sp,
+                    fontWeight: FontWeight.w600,
+                  )
+              ),
+            ],
+          ),
+          SizedBox(height: 123.h,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Image(
+                image: AssetImage('assets/images/Male.png'),
+                width: 138.w,
+                height: 230.h,
+              ),
+            ],
+          ),
+          SizedBox(height: 93.h), // Adjust the spacing between the image and the TextField
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Your Name',
+                hintStyle: GoogleFonts.inter(
+                  fontSize: 15.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black), // Color of the line when the TextField is focused
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black), // Color of the line when the TextField is not focused
+                ),
+                contentPadding: EdgeInsets.all(12.0.sp),
+              ),
+            ),
+          ),
+          SizedBox(height: 113.h,),
+          ContinueButton(),
+        ],
+
+      ),
+
+    );
+  }
+}
